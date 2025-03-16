f541434bc686ae21f77c79192e115100a714ff87
Ivan Bobrov <ibobrovs9@gmail.com>
2025-03-16 20:14:42 +0200

    db shit

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/dbname"  # Замени на свои данные
+DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/dbname"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
