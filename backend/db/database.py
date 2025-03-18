acb23f72ceb71f000f73154f5f8fc956d15fd1db
Ivan Bobrov <ibobrovs9@gmail.com>
2025-03-18 19:31:06 +0200

    db connection

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/dbname"
+DATABASE_URL = "mysql+mysqlconnector://db_user:E4ticigs@localhost:3306/db_manager"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
