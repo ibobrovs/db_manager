11e11e002ee7df068ebfd8ea4e28358d155a6f30
Ivan Bobrov <ibobrovs9@gmail.com>
2025-03-19 20:58:08 +0200

    new migrations made

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://db_user:E4ticigs@localhost:3306/db_manager"
+DATABASE_URL = "mysql+pymysql://db_user:E4ticigs@localhost:3306/db_manager"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()  # <-- Добавляем Base
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
